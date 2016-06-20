7a7b76d0d620ce97d1940074c1c0c0cdb306f0de
Keakone <keanucarnevale@gmail.com>
2016-06-20 13:12:24 +1200

    updated buttons

--- soundcloud-test.js
+++
@@ -1,9 +0,0 @@
-var soundcloud = require("./soundcloud.js")
-
-
-var url = "https://soundcloud.com/teenagedirtbag69/the-ep-before-the-album"
-var callback = function(err,data){
-  // console.log(err,data)
-}
-
-soundcloud(url, callback)
