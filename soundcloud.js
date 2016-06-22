4de2f589d333b98b219ba8361f28edcae2833785
Keakone <keanucarnevale@gmail.com>
2016-06-22 19:58:53 +1200

    update for heroku

--- soundcloud.js
+++ soundcloud.js
@@ -4,13 +4,21 @@ require('fs');
 
 module.exports = function(url,callback) {
   console.log(url, "raw url from form")
-  console.log(process.env.API_KEY, "in soundcloud")
+  console.log(process.env.API_KEY, "got my keys")
+
 
   request.get('http://api.soundcloud.com/resolve?url='+url+'&client_id='+process.env.API_KEY)
+
   .end( function (err, res, data) {
+
+    if (res.body == false) {
+      console.log('OH MY GOD ITS DYING');
+    }
+
     if (err) callback(err)
-    console.log(res.status, 'status')
-    console.log(res.body.user, 'in soundcloud')
+
+
+
     var songObj = {
       "artist":            res.body.user.username,
       "title":             res.body.title,
